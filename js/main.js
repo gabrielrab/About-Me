9062ec273d99315ec1a822b0ba5d1fddefd4e401
Gabriel Rabelo <31012788+omitogabriel@users.noreply.github.com>
2019-01-29 10:34:10 -0200

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed('.prompt', {
+    strings: ['Olá, meu nome é Gabriel Rabelo e sou Desenvolvedor Web.<br>$'],
+    typeSpeed: 50,
+    backSpeed: 10,
+    //fadeOut: true,
+	loop: false
+});
+    
