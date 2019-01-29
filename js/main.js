3ff6645dbf271279ebd1227293363a0f4e0e2953
Gabriel Rabelo <31012788+omitogabriel@users.noreply.github.com>
2019-01-29 16:57:16 -0200

    Adequação

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 
-var typed = new Typed('.prompt', {
-    strings: ['Olá, meu nome é Gabriel Rabelo e sou Desenvolvedor Web.<br>$'],
+var typedInicial = new Typed('.prompt', {
+    strings: ['npm install apresentação^1000<br> `<span class="gray">installing components...</span><br>` ^1000\n `<span class="gray">Fetching from source...</span><br>` ^1000\n `Olá meu nome é Gabriel Rabelo Camargos,<br>Sou Desenvolvedor Web!`'],
     typeSpeed: 50,
-    backSpeed: 10,
+    backSpeed: false,
     //fadeOut: true,
 	loop: false
 });
-    
+
+$('#habilidades').click(function(){
+    $('.prompt').empty();
+    $('.typed-cursor').remove();
+    var typedHab = new Typed('.prompt', {
+        strings: ['cd habilidades^1000<br> `habilidades>` ^1000\n `ls<br>` ^1000\n `&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Front-End => </span>HTML - CSS - JS <br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Back-End =></span> PHP - MySQL - NodeJs - MongoDB<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Desktop =></span> Java - C<span class="gray">(Conhecimento Básico)</span><br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Softwares Gráficos =></span> Photoshop - Illustrator - CorelDraw...<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Extras =></span> GitHub - Wordpress - Upload de Sites - Redes de Computadores - Montagem/Manutenção de Computadores<br>`'],
+        typeSpeed: 50,
+        backSpeed: false,
+        //fadeOut: true,
+        loop: false,
+        insertCursor: false
+    });
+    //$('.typed-cursor').empty();
+});
+
+$('#experiencia').click(function(){
+    $('.terminal').empty();
+    $('.terminal').append('$&nbsp;<span class="prompt"></span>');
+    var typedExp = new Typed('.prompt',{
+        strings: ['cd experiencia^1000<br> `experiencia>` ^1000 `ls<br>` `&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Carmograf(2015-2017) => </span>Designer Gráfico e Arte Finalista <br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Crossfit Divinópolis(2017) => </span>Desenvolvimento de Web Site<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Sistema BUSQUEI(2017-Atualmente) => </span>Desenvolvedor/Proprietário<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Encontro de Professores - Cefet Divinópolis(2018) => </span>Desenvolvedor Back-End<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Sistema SIGEV(2018) => </span>Desenvolvedor<br>&nbsp;&nbsp;`']
+    });
+    $('typed-cursor').empty();
+});
