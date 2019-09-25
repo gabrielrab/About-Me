2f409c97c7972d0b23ff22c21e2877a4458bcb67
Gabriel Rabelo <contato.ideialisa@gmail.com>
2019-09-25 08:06:24 -0300

    Add specs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var typedInicial = new Typed(".prompt", {
   strings: [
-    'npm install apresentação^1000<br> `<span class="gray">installing components...</span><br>` ^1000\n `<span class="gray">Fetching from source...</span><br>` ^1000\n `Olá meu nome é Gabriel Rabelo Camargos,<br>tenho 19 anos,<br>Sou Desenvolvedor Web e estudante de Ciências da Computacào na UTFPR !<br><br><br><br><br><br><br><br><br><br><br><br><span class="gray">Saiba mais clicando nos links abaixo.</span>`'
+    'npm install apresentação^1000<br> `<span class="gray">installing components...</span><br>` ^1000\n `<span class="gray">Fetching from source...</span><br>` ^1000\n `Olá meu nome é Gabriel Rabelo Camargos,<br>tenho 19 anos,<br>Sou Desenvolvedor Web e estudante de Ciências da Computacào na UTFPR !<br><br><br><br><br><br><br><br><br><br><br><span class="gray">Saiba mais clicando nos links abaixo.</span>`'
   ],
   typeSpeed: 50,
   backSpeed: false,
@@ -13,7 +13,7 @@ $("#habilidades").click(function() {
   $(".terminal").append('$&nbsp;<span class="prompt"></span>');
   var typedHab = new Typed(".prompt", {
     strings: [
-      'cd habilidades^1000<br> `habilidades>` ^1000\n `ls<br>` ^1000\n `&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Front-End => </span>ReactJS - HTML - CSS - JS <br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Back-End =></span>NodeJs - MongoDB<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Banco de Dados =></span> MongoDB - MySQL<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Softwares Gráficos =></span> Photoshop - Illustrator - CorelDraw...<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Extras =></span>AWS - Git/GitHub - Wordpress - Infra - Redes de Computadores - Montagem/Manutenção de Computadores<br>`'
+      'cd habilidades^1000<br> `habilidades>` ^1000\n `ls<br>` ^1000\n `&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Front-End => </span>ReactJS - HTML - CSS - JS <br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Back-End =></span>NodeJs - PHP<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Banco de Dados =></span> MongoDB - MySQL<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Softwares Gráficos =></span> Photoshop - Illustrator - CorelDraw...<br>&nbsp;&nbsp;<span class="green">✓</span> <span class="gray">Extras =></span>AWS - Git/GitHub - Wordpress - Infra - Redes de Computadores - Montagem/Manutenção de Computadores<br>`'
     ],
     typeSpeed: 50,
     backSpeed: false,
